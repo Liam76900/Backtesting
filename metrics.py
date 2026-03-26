52ac5f04d53772b73dceba845034d8aae4aa4fd2
Liam76900 <ljthomson@gmail.com>
2026-03-26 01:37:33 +0000

    Added method of calculating Annualised Volatility

--- metrics.py
+++ metrics.py
@@ -3,3 +3,5 @@ import numpy as np
 def calculate_metrics(data):
 
     sharpe_ratio=(data["Strategy_Returns"].mean() / data["Strategy_Returns"].std()) * np.sqrt(252)
+
+    annualised_volatility=(data["Strategy_Returns"].std() * np.sqrt(252))
