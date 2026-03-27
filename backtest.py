22e597740e995455b0a8dce0e61e6fdd1e0576d9
Liam76900 <ljthomson@gmail.com>
2026-03-27 21:27:53 +0000

    Added transaction costs for strategy and buy and hold strategy

--- backtest.py
+++ backtest.py
@@ -1,11 +1,18 @@
 def backtesting(data):
     data["Strategy_Returns"]=data["Signal"] * data["Return"]
-<<<<<<< HEAD
-    
-=======
+    data["Trade"]=data["Signal"].diff().abs()
 
->>>>>>> 5caa5b849eb32e1e16a62febca2176c8ff6afe95
+    cost=0.001
+
+    data["Transaction_Costs"]=data["Trade"] * cost
+
+    data["Net_Returns"]=data["Strategy_Returns"]-data["Transaction Costs"]
+
+    data["Market_Transaction_Costs"]=0
+    data.iloc(0, data.columns.get_loc("Market_Transaction_Costs")) = cost
+    data.iloc(-1, data.columns.get_loc("Market_Transaction_Costs")) = cost
+    data["Net_Market_Returns"]=data["Returns"] - data["Market_Transaction_Costs"]
     data["Cumulative_Market"]=(1+data["Return"]).cumprod()
-    data["Cumulative_Strategy"]=(1+data["Strategy_Returns"]).cumprod()
+    data["Cumulative_Strategy"]=(1+data["Net Returns"]).cumprod()
 
     return data
