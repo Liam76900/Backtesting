ea6cb44bd6414872d88a786415fb2312c0bf5cf2
Liam76900 <ljthomson@gmail.com>
2026-03-28 01:11:44 +0000

    Added comments

--- metrics.py
+++ metrics.py
@@ -2,9 +2,15 @@ import numpy as np
 
 def calculate_metrics(data):
 
+    #Computes sharpe ratio to analyse risk-adjusted returns
+
     sharpe_ratio=(data["Net_Strategy_Returns"].mean() / data["Net_Strategy_Returns"].std()) * np.sqrt(252)
 
+    #Computes annualised volatility to quantify risk
+
     annualised_volatility=(data["Net_Strategy_Returns"].std() * np.sqrt(252))
+
+    #Computes maximum drawdown to indicate the greatest possible loss
 
     data["Rolling_Max"]=data["Cumulative_Strategy"].cummax()
     data["Drawdown"]=data["Cumulative_Strategy"] / data["Rolling_Max"] - 1
